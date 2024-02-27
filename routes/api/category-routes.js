a26e9d6e8de12b62418db454a7aa644d8c3e0132
waltermatulich <wmatulich231@gmail.com>
2024-02-26 17:29:01 -0800

    moved all file folders and updated js files

---
+++ routes/api/category-routes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+    try {
+  const categoryData = await Category.findAll({
+    include: [Product]
+  });
+
+  if(!categoryData){
+    res.status(400).json({ message: "no categories"})
+    return
+  }
+
+  res.status(200).json(categoryData)
+} catch(err) {
+  res.status(500).json(err);
+}
+
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [Product]
+    });
+  
+    if(!categoryData){
+      res.status(400).json({ message: "no categories with that id"})
+      return
+    }
+  
+    res.status(200).json(categoryData)
+  } catch(err) {
+    res.status(500).json(err);
+  }
+  
+  })
+  // find one category by its `id` value
+  // be sure to include its associated Products
+
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +9,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use("/api");
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
+sequelize.sync({ force: false }) .then(() => {
+  app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+});
 });
